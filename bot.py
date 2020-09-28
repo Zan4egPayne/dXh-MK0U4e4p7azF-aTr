527d8858d92d3c0e49bb88e5c4e44b10c0e74e03
Zan4egPayne <43988120+Zan4egPayne@users.noreply.github.com>
2020-09-28 20:33:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,6 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import json, socket, threading, time, concurrent.futures
 from six.moves import urllib
-import requests
 
 
 PREFIX = 'i.' # Переменная префикса
